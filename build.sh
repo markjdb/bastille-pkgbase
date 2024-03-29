682723707e4840f9da7ef1b92f123089af5d46ae
Mark Johnston <markjdb@gmail.com>
2024-03-29 09:36:46 -0400

    Fix a few bugs

--- build.sh
+++ build.sh
@@ -13,10 +13,12 @@ git pull
 
 jflag=-j$(sysctl -n hw.ncpu)
 
-nice -n 20 make -s -j$(sysctl -n hw.ncpu) buildworld
-PORTSDIR=/root/ports UNAME_r=13.0-RELEASE make -s $jflag buildkernel LOCAL_MODULES="$MODLIST"
-PORTSDIR=/root/ports UNAME_r=13.0-RELEASE make $jflag packages LOCAL_MODULES="$MODLIST"
+VERSION=$(. /etc/os-release; echo $VERSION)
+
+nice -n 20 make -s $jflag buildworld
+PORTSDIR=/root/ports UNAME_r=$VERSION make -s $jflag buildkernel LOCAL_MODULES="$MODLIST"
+PORTSDIR=/root/ports UNAME_r=$VERSION make $jflag packages LOCAL_MODULES="$MODLIST"
 
 nice -n 20 make -s $jflag buildworld TARGET=arm64
-PORTSDIR=/root/ports UNAME_r=13.0-RELEASE make -s $jflag buildkernel TARGET=arm64
-PORTSDIR=/root/ports UNAME_r=13.0-RELEASE make $jflag packages PKG_FORMAT=zst TARGET=arm64
+PORTSDIR=/root/ports UNAME_r=$VERSION make -s $jflag buildkernel TARGET=arm64
+PORTSDIR=/root/ports UNAME_r=$VERSION make $jflag packages PKG_FORMAT=zst TARGET=arm64
